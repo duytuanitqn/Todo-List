ca0252daed91f2af12c64e11b4e1c636244e1fd9
duytuanitqn <nguyenduytuandt13ctt02@gmail.com>
2021-03-19 13:31:30 +0700

    feature/sprint1 show todo list

--- modules/TodoList/Controller/TodoListController.php
+++ modules/TodoList/Controller/TodoListController.php
@@ -3,9 +3,27 @@
 namespace Modules\TodoList\Controller;
 
 use Core\Controller\BaseController;
+use Modules\TodoList\Models\TodoList;
 
 class TodoListController extends BaseController
-{ 
+{
+    const DEFAULT_CURRENT_PAGE = 1;
+    const DEFAULT_LIMIT_PAGE = 10;
+
+    protected $todoList;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // init todo list model
+        $newTodoList = new TodoList();
+        $this->todoList = $newTodoList;
+    }
+
     /**
      * Display todo list
      *
@@ -14,10 +32,30 @@ class TodoListController extends BaseController
     public function index()
     {
         try {
-            $this->loadView("TodoList\Views\index.html");
+            $currentPage = isset(app('request')->query['page']) ? app('request')->query['page'] : $this::DEFAULT_CURRENT_PAGE;
+            $totalRecords = $this->todoList->countTotalRecord();
+            $data = [];
+            if ($totalRecords > 0) {
+                $totalPage = ceil($totalRecords / $this::DEFAULT_LIMIT_PAGE);
+                if ($currentPage > $totalPage){
+                    $currentPage = $totalPage;
+                }else if ($currentPage < $this::DEFAULT_CURRENT_PAGE){
+                    $currentPage = $this::DEFAULT_CURRENT_PAGE;
+                }
+                $start = ($currentPage - 1) * $this::DEFAULT_LIMIT_PAGE;
+                $data = [
+                    'results' => $this->todoList->paginate($start, $this::DEFAULT_LIMIT_PAGE),
+                    'total_record' => $totalRecords,
+                    'total_page' => $totalPage,
+                    'current_page' => $currentPage,
+                    'previous' => $currentPage - 1,
+                    'next' => $currentPage + 1
+                ];
+            }
+            $this->loadView("TodoList\Views\index.html", $data);
         } catch (Exception $e) {
             echo $e->getMessage();
             die();
         }
     }
-} 
+}
