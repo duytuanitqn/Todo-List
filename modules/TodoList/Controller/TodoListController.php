8ce7e8c1ce2706e473af0961bad3959f464b1b68
duytuanitqn <nguyenduytuandt13ctt02@gmail.com>
2021-03-19 13:48:29 +0700

    feature/sprint1 implement create new todo list

--- modules/TodoList/Controller/TodoListController.php
+++ modules/TodoList/Controller/TodoListController.php
@@ -4,6 +4,7 @@ namespace Modules\TodoList\Controller;
 
 use Core\Controller\BaseController;
 use Modules\TodoList\Models\TodoList;
+use Core\Helper\Validate;
 
 class TodoListController extends BaseController
 {
@@ -12,6 +13,8 @@ class TodoListController extends BaseController
 
     protected $todoList;
 
+    protected $validate;
+
     /**
      * Create a new controller instance.
      *
@@ -19,9 +22,15 @@ class TodoListController extends BaseController
      */
     public function __construct()
     {
+        // start session
+        session_start();
         // init todo list model
         $newTodoList = new TodoList();
         $this->todoList = $newTodoList;
+
+        // init validate
+        $newValidate = new Validate();
+        $this->validate = $newValidate;
     }
 
     /**
@@ -57,5 +66,93 @@ class TodoListController extends BaseController
             echo $e->getMessage();
             die();
         }
+    }
+
+    /**
+     * Display view create a new todo list.
+     *
+     * @return void
+     */
+    public function create()
+    {
+        try {
+            $error = array();
+            if(isset($_SESSION["errors"])){
+                $error = $_SESSION["errors"];
+                unset($_SESSION["errors"]);
+            }
+            $data = [
+                'errors' => $error
+            ];
+            $this->loadView("TodoList\Views\create.html", $data);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            die();
+        }
+    }
+
+    /**
+     * Create a new todo list.
+     *
+     * @return void
+     */
+    public function store()
+    {
+        try {
+            if($_SERVER["REQUEST_METHOD"] == "POST") {
+                $workName = $this->validate->trimInput(app('request')->body['work_name']);
+                $startingDate = $this->validate->trimInput(app('request')->body['starting_date']);
+                $endingDate = $this->validate->trimInput(app('request')->body['ending_date']);
+                $errorValidate = $this->validateRequestCreateTodoList($workName, $startingDate, $endingDate);
+                if(!empty($errorValidate)) {
+                    $_SESSION["errors"] = $errorValidate;
+                    $this->redirect('/todo-list/create');
+                } else {
+                    $args = [
+                        "work_name" => $workName,
+                        "starting_date" => $startingDate,
+                        "ending_date" => $endingDate
+                    ];
+                    $this->todoList->createTodoList($args);
+                    $this->redirect('/todo-list/index');
+                }
+            } else {
+                echo "Method not allow";
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            die();
+        }
+    }
+
+    /**
+     * Validate request create new todo list.
+     *
+     * @param $workName     string [work name]
+     * @param $startingDate date   [starting date]
+     * @param $endingDate   date   [ending date]
+     *
+     * @return array
+     */
+    private function validateRequestCreateTodoList($workName, $startingDate, $endingDate)
+    {
+        $error = array();
+        if (!$this->validate->required($workName)) {
+            $error['work_name'] = 'The work name field is required.';
+        }
+        if (!$this->validate->required($startingDate)) {
+            $error['starting_date'] = 'The starting date field is required.';
+        } else if (!$this->validate->date($startingDate)) {
+            $error['starting_date'] = 'The starting date is invalid format.';
+        }
+        if (!$this->validate->required($endingDate)) {
+            $error['ending_date'] = 'The ending date field is required.';
+        } else if (!$this->validate->date($endingDate)) {
+            $error['ending_date'] = 'The ending date is invalid format.';
+        }
+        if (strtotime($startingDate) > strtotime($endingDate)) {
+            $error['starting_date'] = 'The start date must be less than the end date.';
+        }
+        return $error;
     }
 }
